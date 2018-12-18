2024960a72ee889ec66e48a24acd1089b48a56a2
no1canlove <no1canlove@gmail.com>
2018-12-18 19:27:07 +0700

    commit migration and repository

---
+++ HireYourGuide.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using HireYourGuide.Data.Infrastructure;
+using HireYourGuide.Model.Models;
+
+namespace HireYourGuide.Data.Repositories
+{
+    public interface IproductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
